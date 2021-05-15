440b4d5204f3a2df7ba82870e00fcb2622ef9d8f
Carlos Alberto de Oliveira <ca.oliveira0265@gmail.com>
2021-05-15 16:27:02 -0300

    Estado e cidade

---
+++ src/main/java/com/carlos/curso_mc/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.carlos.curso_mc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carlos.curso_mc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
